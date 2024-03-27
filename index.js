ce7f3ed6c1589b610a8611ba46c97d550fce4e51
grace ngure <wnguregrace@gmail.com>
2024-03-27 07:05:40 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(record){
+    const result =record.find(record =>record.result==="W")
+    if (!!result){
+        return result.year
+    }else{
+        undefined
+    }
+}
